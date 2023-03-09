3c8d1e8a0d72666f2bd343a2284cc6a3660eb916
LizaBinar <106739245+LizaBinar@users.noreply.github.com>
2023-03-09 23:30:23 +0700

    initial commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+import '../scss/style.scss'
+
+import './servicesExpand.js'
+import './repairsExpand.js'
+import './servicesSwiper.js'
+import './repairsSwiper.js'
+import './pricesSwiper.js'
+import './showMenu.js'
+import './orderCallShow.js'
+import './feedbackShow.js'
+import './aside.js'
+import './glass.js'
+
