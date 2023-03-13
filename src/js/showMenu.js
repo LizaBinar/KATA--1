401f979668dd725e83d9233f5e37da57d2bc25a5
LizaBinar <106739245+LizaBinar@users.noreply.github.com>
2023-03-13 20:10:28 +0700

    main fix

--- src/js/showMenu.js
+++
@@ -1,9 +0,0 @@
-let button = document.querySelector('.menu-button')
-const menu = document.querySelector('.aside')
-
-function  flexNav() {
-  console.log("flexNav")
-  menu.classList.toggle("show")
-}
-
-button.addEventListener('click', flexNav);
